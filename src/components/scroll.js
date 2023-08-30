ab7f26f7efcee9477198c5a5572408762d251d56
baseer <baseerr616@gmail.com>
2023-08-30 20:06:30 +0500

    adjusted scroll height

--- src/components/scroll.js
+++ src/components/scroll.js
@@ -1,6 +1,6 @@
 const Scroll = (props) => {
     return (
-        <div style={{overflowY: 'scroll', border: '1px solid black', height: '530px'}}>
+        <div style={{overflowY: 'scroll', border: '1px solid black', height: '800px'}}>
             {props.children}
         </div>
     );
